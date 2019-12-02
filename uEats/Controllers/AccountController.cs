52d1f5587394c6322e67ad7d9ddb0a8d33536834
shunjid <shunjid1862@gmail.com>
2019-12-03 00:27:05 +0600

    initiated log/reg design

---
+++ uEats/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using uEats.Models;
+
+namespace uEats.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<CustomUser> _userManager;
+        private readonly SignInManager<CustomUser> _signInManager;
+
+        public AccountController(ApplicationDbContext context, UserManager<CustomUser> userManager, SignInManager<CustomUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        
+        /*
+         * HttpGet : When request comes to UserAccount/Register
+         * this page's View will be shown
+         */
+        [HttpGet]
+        public IActionResult Register(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        /*
+         * HttpGet : When request comes to UserAccount/Login
+         * this page's View will be shown
+         */
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        
+        
+    }
+}
---
+++ uEats/Data/AuthUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace uEats.Data
+{
+    public class AuthUser
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserEmail { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string UserPassword { get; set; }
+    }
+}
