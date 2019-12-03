622ab6c2b12d145e9096d33ca47f6cf59aaef813
shunjid <shunjid1862@gmail.com>
2019-12-04 00:32:45 +0600

    added new procedured

---
+++ uEats/Data/Proc.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE get_sp_AllAvailableLocations
+AS
+BEGIN
+    SELECT * FROM Locations;
+END
+go
+
+
+
+CREATE PROCEDURE sp__UpgradeRestaurantInformation
+@restaurantId nvarchar(max),
+@restaurantName nvarchar(max),
+@locationId int
+AS
+BEGIN
+    UPDATE Restaurants
+    SET RestaurantName = @restaurantName, LocationId = @locationId
+    WHERE RestaurantId = @restaurantId;
+    
+    SELECT * FROM Restaurants WHERE RestaurantId = @restaurantId;
+END
+go
+
